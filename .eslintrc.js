05d284bfbcacfe81bb524ffa0f14d05129dc55cd
John Drefahl <john@velobpa.com>
2026-02-24 05:53:32 -0600

    chore: update .eslintrc.js to v1.0.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,15 @@
 /*
- * Copyright (c) Velocity BPA, LLC
+ * Copyright (c) 2026 Velocity BPA, LLC
  * Licensed under the Business Source License 1.1
- * Commercial use requires a separate commercial license.
- * See LICENSE file for details.
  */
 
 module.exports = {
-	root: true,
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		project: './tsconfig.json',
-		sourceType: 'module',
-		ecmaVersion: 2020,
-	},
-	plugins: ['@typescript-eslint'],
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-	],
-	env: {
-		node: true,
-		es2020: true,
-	},
-	rules: {
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-		'@typescript-eslint/explicit-function-return-type': 'off',
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/ban-ts-comment': 'off',
-		'no-console': 'warn',
-		'prefer-const': 'error',
-		'no-var': 'error',
-	},
-	ignorePatterns: ['dist/', 'node_modules/', '*.js'],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['eslint-plugin-n8n-nodes-base'],
+  extends: ['plugin:n8n-nodes-base/community'],
+  ignorePatterns: ['node_modules/**', 'dist/**'],
+  rules: {
+    'n8n-nodes-base/community-package-json-name-still-default': 'off',
+  },
 };
